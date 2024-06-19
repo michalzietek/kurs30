48ba146b155a818c3ea4814a435646a3f0a8d521
Michał Ziętkowski <michal.zietkowski@stxnext.pl>
2024-06-19 17:30:19 +0200

    fix

--- zajecia_10_zadanie/welcome.py
+++ zajecia_10_zadanie/welcome.py
@@ -1,11 +1,13 @@
+from datetime import datetime
+
 #TODO stworz 2 funkcje odpowiadajace za wczytanie z pliku i zapisanie do pliku
 
 imie = input("Podaj imię osoby obchodzącej urodziny: ")
 rok = input("Podaj rocznik urodzin: ")
 autor = input("Podaj swoje imię: ")
-
+wiek = datetime.now().year - int(rok)
 #templatka = wczytaj_templatke()
-wypelniona_templatka = templatka.format(imie=imie, rok=rok, autor=autor)
+wypelniona_templatka = templatka.format(imie=imie, rok=wiek, autor=autor)
 
 nazwa_pliku = input("Podaj nazwę pliku do zapisu: ")
 #zapisz_zyczenia(nazwa_pliku, wypelniona_templatka)
